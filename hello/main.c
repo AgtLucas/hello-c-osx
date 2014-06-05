3a5ad5feca6a5ba48fd31572628bbe009c0272db
Lucas Churchill <lucas@agtlucas.com>
2014-06-05 18:16:51 -0300

    Updates

---
+++ hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Lucas Churchill on 6/5/14.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
